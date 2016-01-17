c15937595ac4a2f7fe82e33728c9983960eec043
chathurangasandun <chathurangasandukumara@gmail.com>
2016-01-18 01:18:21 +0530

    Initial commit

---
+++ app/src/main/java/com/clivekumara/thenewbostonexample2splash/Menu.java
@@ -0,0 +1,22 @@
+package com.clivekumara.thenewbostonexample2splash;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+/**
+ * Created by chathuranga on 1/18/2016.
+ */
+public class Menu extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+    }
+}
---
+++ app/src/main/java/com/clivekumara/thenewbostonexample2splash/SplashActivity.java
@@ -0,0 +1,56 @@
+package com.clivekumara.thenewbostonexample2splash;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class SplashActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // create object
+        mediaPlayer = MediaPlayer.create(this, R.raw.sound);
+        mediaPlayer.start();
+
+        Thread timer = new Thread(){
+            /**
+             * Calls the <code>run()</code> method of the Runnable object the receiver
+             * holds. If no Runnable is set, does nothing.
+             *
+             * @see Thread#start
+             */
+            @Override
+            public void run() {
+
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        };
+
+        timer.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.release();
+        finish();
+    }
+}
